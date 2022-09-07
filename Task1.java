dc8cf7f8ed907200d144404aff4ecc6744b2c66e
Alex <112649821+Ayako-q@users.noreply.github.com>
2022-09-07 23:21:54 +0300

    Create Task1.java

---
+++ Task1.java
@@ -0,0 +1,11 @@
+package com.example.project_;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("My name is Oleksii Welcome to my first Java program");
+        System.out.println("I'm going to add some more text here");
+        System.out.println("Just to make sure everything is ok");
+        //testing IntelliJ IDEA
+    }
+}
